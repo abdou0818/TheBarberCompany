2b25c9d1abab815049146a2c80fe20dca109d677
abdou0818 <abdellahbensania818@gmail.com>
2026-01-05 18:45:29 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,3 +1,4 @@
+// firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
@@ -5,14 +6,15 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-
 const firebaseConfig = {
   apiKey: "AIzaSyBA4lhTkkLBZWYYqMw-FY1jEKETaKFymjQ",
   authDomain: "the-barber-company-302a4.firebaseapp.com",
-  databaseURL: "https://the-barber-company-302a4-default-rtdb.firebaseio.com",
+
+  ✅ databaseURL: "https://the-barber-company-302a4-default-rtdb.firebaseio.com",
+
   projectId: "the-barber-company-302a4",
   storageBucket: "the-barber-company-302a4.appspot.com",
   messagingSenderId: "1001421518378",
   appId: "1:1001421518378:web:5ff4852c39736e58a8f553"
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 export const storage = getStorage(app);
