241d33d7e0f3de20360ad6ceab3b744eaf9bc11e
abdou0818 <abdellahbensania818@gmail.com>
2026-01-05 17:41:58 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,21 +1,18 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBA4lhTkkLBZWYYqMw-FY1jEKETaKFymjQ",
   authDomain: "the-barber-company-302a4.firebaseapp.com",
+  databaseURL: "https://the-barber-company-302a4-default-rtdb.firebaseio.com",
   projectId: "the-barber-company-302a4",
-  storageBucket: "the-barber-company-302a4.firebasestorage.app",
+  storageBucket: "the-barber-company-302a4.appspot.com",
   messagingSenderId: "1001421518378",
-  appId: "1:1001421518378:web:5ff4852c39736e58a8f553",
-  measurementId: "G-X6MHRDF6RQ"
+  appId: "1:1001421518378:web:5ff4852c39736e58a8f553"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const storage = getStorage(app);
