19ee275e154e1b1e82b631c07e6f1a0b7da80573
abdou0818 <abdellahbensania818@gmail.com>
2026-01-05 17:40:52 +0100

    Remove save-settings.php file

--- save-settings.php
+++ save-settings.php
@@ -1,9 +1,1 @@
-<?php
-$data = file_get_contents('php://input');
-if($data){
-    file_put_contents('settings.json',$data);
-    echo "تم حفظ التغييرات للزوار!";
-} else {
-    echo "لم يتم استلام البيانات!";
-}
-?>
+
