e2aead81093d4a97fb43d495053fada6ea534327
abdou0818 <abdellahbensania818@gmail.com>
2026-01-05 19:54:21 +0100

    Switch from Firestore to Realtime Database

--- firebase-init.js
+++ firebase-init.js
@@ -1,16 +1,18 @@
+// تأكد من تغيير القيم بقيم مشروعك الخاص على Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBA4lhTkkLBZWYYqMw-FY1jEKETaKFymjQ",
-  authDomain: "the-barber-company-302a4.firebaseapp.com",
-  projectId: "the-barber-company-302a4",
-  storageBucket: "the-barber-company-302a4.appspot.com",
-  messagingSenderId: "1001421518378",
-  appId: "1:1001421518378:web:5ff4852c39736e58a8f553"
+  apiKey: "ضع هنا مفتاح API",
+  authDomain: "ضع هنا authDomain",
+  databaseURL: "ضع هنا رابط قاعدة البيانات", // مهم جدًا لتجنب خطأ 404
+  projectId: "ضع هنا projectId",
+  storageBucket: "ضع هنا storageBucket",
+  messagingSenderId: "ضع هنا messagingSenderId",
+  appId: "ضع هنا appId"
 };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 export const storage = getStorage(app);
